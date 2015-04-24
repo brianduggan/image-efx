1aa4073a68e5bd085be17f3e0b058f3f450e2324
brianduggan <brian.e.duggan@icloud.com>
2015-04-23 20:19:32 -0400

    refactored code, simplified class structure

--- js/image-efx.js
+++ js/image-efx.js
@@ -1,19 +1,19 @@
 $(function(){
 
 	function crossFadeMouseOn() {
-		$(this).find('.front').fadeTo(400, 0);
+		$(this).find('img').eq(0).fadeTo(400, 0);
 	}
 
 	function crossFadeMouseOff() {
-		$(this).find('.front').fadeTo(400, 1);
+		$(this).find('img').eq(0).fadeTo(400, 1);
 	}
 
 	$('.crossfade-frame').hover(crossFadeMouseOn, crossFadeMouseOff);
 
 	$('.slide-frame').hover(function(){
-		$(this).find('.top').slideUp("slow");
+		$(this).find('img').eq(0).slideUp("slow");
 	}, function(){
-		$(this).find('.top').slideDown("fast");
+		$(this).find('img').eq(1).slideDown("fast");
 	});
 
 	$('.caption-frame').hover(function(){
